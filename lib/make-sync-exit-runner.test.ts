bf897077378e802609459e936389b5d31c7d56df
hideyuki-hori <montecarlo1101@gmail.com>
2025-05-18 22:34:14 +0900

    refactor

--- lib/make-sync-exit-runner.test.ts
+++ lib/make-sync-exit-runner.test.ts
@@ -1,7 +1,7 @@
 import { Effect, Exit } from 'effect'
 import { describe, it, expect } from 'vitest'
-import { makeSyncExitRunner } from './make-sync-exit-runner'
 import { unreachable } from '~test-utils/unreachable'
+import { makeSyncExitRunner } from './make-sync-exit-runner'
 
 describe('makeSyncExitRunner', () => {
   it('returns Exit.success when Effect succeeds', () => {
