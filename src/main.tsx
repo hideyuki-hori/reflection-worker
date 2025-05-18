d799e98d99d2c848b082088ac9cb047f62268c60
hideyuki-hori <montecarlo1101@gmail.com>
2025-05-18 09:56:45 +0900

    chore: customize scaffold

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,4 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-import { Stack } from 'panda/jsx'
-import './panda.css'
-
-const root = document.getElementById('__REFRECTION_WORKER__')
-
-render(() => <Stack direction='row' gap='20'>
-  <div>aaa</div>
-  <div>aaa</div>
-  <div>aaa</div>
-</Stack>, root!)
+import './global.css'
+import { findOne } from './lib/dom'
